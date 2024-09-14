093573f03d3ca508681da2f8f05187b02abcd317
emfvxkoodaaja <etunimis321@gmail.com>
2024-09-14 14:45:11 +0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+author 'emfvxkoodaaja'
+description 'This script shows your FiveM players weapons on its back'
+client_script 'cl_weapons-on-back.lua'
